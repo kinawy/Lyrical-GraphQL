cf828cb689fc92921e490d5dca3eded769483d29
kinawy <sameh.yasser.kinawy@gmail.com>
2020-09-04 11:07:41 -0400

    Adds min functionality

--- client/index.js
+++ client/index.js
@@ -2,9 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import ApolloClient from 'apollo-client'
 import { ApolloProvider } from 'react-apollo'
+// THIS IS MIN CONFIG FOR APOLLO/ REACT, YOU SHOULD BE ABLE TO DO QUERIES AS IS
+const client = new ApolloClient({})
 
 const Root = () => {
   return (
+    // the glue between react and graphql/apollo world, its shovelling data over to the react side of the application
     <ApolloProvider client={client}>
     <div>Lyrical</div>
     </ApolloProvider>
