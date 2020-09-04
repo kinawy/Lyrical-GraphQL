847c4ea54b1f4c544e917808cbb909a3cb8e9c42
kinawy <sameh.yasser.kinawy@gmail.com>
2020-09-04 11:05:10 -0400

    Adds all thats required for apollo provider

--- client/index.js
+++ client/index.js
@@ -1,8 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import ApolloClient from 'apollo-client'
+import { ApolloProvider } from 'react-apollo'
 
 const Root = () => {
-  return <div>Lyrical</div>
+  return (
+    <ApolloProvider client={client}>
+    <div>Lyrical</div>
+    </ApolloProvider>
+  )
 };
 
 ReactDOM.render(
--- server/server.js
+++ server/server.js
@@ -1,20 +1,25 @@
 const express = require('express');
+require('dotenv').config()
 const models = require('./models');
 const expressGraphQL = require('express-graphql');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const schema = require('./schema/schema');
-
+const SAMEH = process.env.SAMEH
 const app = express();
 
 // Replace with your mongoLab URI
-const MONGO_URI = '';
+const MONGO_URI = `mongodb+srv://kinawy:${SAMEH}@sameh-mongo-cluster.gjtw5.mongodb.net/<dbname>?retryWrites=true&w=majority`;
 if (!MONGO_URI) {
   throw new Error('You must provide a MongoLab URI');
 }
 
 mongoose.Promise = global.Promise;
-mongoose.connect(MONGO_URI);
+mongoose.connect(MONGO_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useFindAndModify: true
+});
 mongoose.connection
     .once('open', () => console.log('Connected to MongoLab instance.'))
     .on('error', error => console.log('Error connecting to MongoLab:', error));
